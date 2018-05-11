817c7969fffcf15a29fca83968af5050b5f860fc
Cloud User <ec2-user@ip-172-30-0-18.eu-west-1.compute.internal>
2018-05-11 14:02:43 +0000

    working install script

--- files/bootme.sh
+++ files/bootme.sh
@@ -1,19 +1,24 @@
-PROVISIONING_REPO=lgi_provisoning.git
+PROVISIONING_REPO=lgi_provisioning
 REPO_PROVIDER=https://github.com/pgomersbach
 OS_MAJOR=7
+TERRAFORM_VERSION=0.11.7
+PACKER_VERSION=1.2.3
 
 ## Code start ##
-# check admin
+# check root
 if [ "$(id -u)" != "0" ]; then
-    echo "This script must be run as root." >&2
-    exit 1
+    # no roor, check sudo
+    if ! timeout 2 sudo id > /dev/null; then 
+        echo "This script must be run as root or sudo access." >&2
+        exit 1
+    fi
 fi
 
 function provision_rhel() {
     # install dependencies
-    sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+    sudo yum install -y -q https://dl.fedoraproject.org/pub/epel/epel-release-latest-${RH_MAJOR}.noarch.rpm 2>/dev/null
     sudo yum update -y -q
-    sudo yum install -y -q git ansible
+    sudo yum install -y -q git ansible unzip
 }
 
 # check os and release
@@ -33,10 +38,22 @@ else
     exit 1
 fi 
 
-# clone or update repo
-git clone ${REPO_PROVIDER}/${PROVISIONING_REPO} 
+# get or update repo
+if [ -d $PROVISIONING_REPO ]; then
+  echo "Update repo"
+  cd $PROVISIONING_REPO
+  git pull
+else
+  echo "Cloning repo"
+  git clone ${REPO_PROVIDER}/${PROVISIONING_REPO} $PROVISIONING_REPO
+  cd $PROVISIONING_REPO
+fi
 
-# install terraform using ansible
+# install terraform
+curl https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip -s -o /tmp/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+sudo unzip -o -q /tmp/terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /usr/local/bin/
 
-# install packer using ansible
+# install packer
+curl https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip -s -o /tmp/packer_${PACKER_VERSION}_linux_amd64.zip
+sudo unzip -o -q /tmp/packer_${PACKER_VERSION}_linux_amd64.zip -d /usr/local/bin/
 
