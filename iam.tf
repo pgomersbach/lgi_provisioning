5fd23fbc0ab4bf00266f0a8d825ad80fabf49fe4
Cloud User <ec2-user@ip-172-31-38-204.eu-central-1.compute.internal>
2018-06-15 11:42:01 +0000

    add iam role to enable logging add logging to cloudwatch client add outbound traffic to security group

---
+++ iam.tf
@@ -0,0 +1,54 @@
+resource "aws_iam_instance_profile" "cloudwatch_profile" {
+  name = "cloudwatch_profile"
+  role = "${aws_iam_role.cloudwatch_role.name}"
+}
+
+resource "aws_iam_role" "cloudwatch_role" {
+  name = "cloudwatch_role"
+  path = "/"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+               "Service": "ec2.amazonaws.com"
+            },
+            "Effect": "Allow",
+            "Sid": ""
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "awslogs_iam_role_inline_policy" {
+  name = "awslogs_iam_role_policy"
+  role = "${aws_iam_role.cloudwatch_role.id}"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "AllowPublishToCloudwatchLogs",
+      "Effect": "Allow",
+      "Action": [
+        "cloudwatch:PutMetricData",
+        "ec2:DescribeTags",
+        "logs:PutLogEvents",
+        "logs:DescribeLogStreams",
+        "logs:DescribeLogGroups",
+        "logs:CreateLogStream",
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream"
+    ],
+      "Resource": [
+        "*"
+    ]
+  }
+ ]
+}
+EOF
+}
--- sg.tf
+++ sg.tf
@@ -13,6 +13,12 @@ resource "aws_security_group" "app_websg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_security_group" "mon_sg" {
