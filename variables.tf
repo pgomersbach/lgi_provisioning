4729076c799635dba7964e63000e37273ccbc053
Cloud User <ec2-user@ip-172-31-38-56.eu-central-1.compute.internal>
2018-07-04 11:51:08 +0000

    change to dev ip address comment on variables change dev instance types

--- variables.tf
+++ variables.tf
@@ -1,55 +1,11 @@
+##### do not put keys in this file but use environment variables for access and secret access keys #####
 variable "access_key" {}
+
 variable "secret_key" {}
 
-variable "hosted_zone" {
-  description = "predefined hosted zone"
-  default     = "api.cloud.libertyglobal.upc.biz"
-}
-
-variable "jump_username" {
-  default = "ec2-user"
-}
-
-variable "app_username" {
-  default = "ec2-user"
-}
-
-variable "ssh_key_file" {
-  default = "~/.ssh/id_rsa.terraform"
-}
-
-variable "region" {
-  description = "EC2 Region for the VPC"
-  default     = "eu-central-1"
-}
-
-variable "app_ami_name" {
-  description = "Name of the ami to use"
-  default     = "packer-app*"
-}
-
-variable "jump_ami_name" {
-  description = "Name of the ami to use"
-  default     = "packer-jump*"
-}
-
-variable "rapid7_ami_name" {
-  description = "Name of the rapid7 ami"
-  default     = "nexpose-pre-authorized-engine-*"
-}
-
-variable "ami_owner" {
-  description = "Owner of the ami"
-  default     = "536449617726"
-}
-
-variable "rapid7_ami_owner" {
-  description = "Owner of the ami"
-  default     = "679593333241"
-}
-
+##### rapid7 scan engine settings #####
 variable "rapid7_console_address" {
-  description = "Rapid 7 console"
+  description = "Rapid 7 console address"
   default     = "r7console"
 }
 
@@ -63,14 +19,53 @@ variable "rapid7_console_secret" {
   default     = "changeme"
 }
 
-variable "api_instance_count" {
-  description = "Instance count of api instances"
-  default     = 1
+##### change these depending on environment #####
+variable "environment" {
+  description = "Environment to deploy"
+  default     = "dev"
 }
 
+variable "region" {
+  description = "EC2 Region for the VPC"
+  default     = "eu-central-1"
+}
+
+variable "vpc_azs" {
+  description = "Availability zones to use"
+  default     = ["eu-central-1a", "eu-central-1b"]
+}
+
+variable "vpc_cidr" {
+  description = "VPC cidr"
+  default     = "172.19.8.0/25"
+}
+
+variable "vpc_private_subnets" {
+  description = "Private subnets cidr"
+  default     = ["172.19.8.0/27", "172.19.8.32/27"]
+}
+
+variable "vpc_public_subnets" {
+  description = "Public subnets cidr"
+  default     = ["172.19.8.64/27", "172.19.8.96/27"]
+}
+
+##### change only if using a diferent ssh key #####
+variable "ssh_key_file" {
+  description = "ssh key to access instances"
+  default     = "~/.ssh/id_rsa.terraform"
+}
+
+##### ajust to scale the number of API servers #####
+variable "api_instance_count" {
+  description = "Instance count of api instances"
+  default     = 2
+}
+
+##### ajust to change instance types #####
 variable "api_instance_type" {
   description = "Instance type of api instances"
-  default     = "t2.micro"
+  default     = "m5.large"
 }
 
 variable "jump_instance_type" {
@@ -83,23 +78,43 @@ variable "rapid7_instance_type" {
   default     = "t2.large"
 }
 
-variable "environment" {
-  description = "Environment to deploy"
-  default     = "dev"
+##### No need to change variables below this point #####
+variable "hosted_zone" {
+  description = "predefined hosted zone"
+  default     = "api.cloud.libertyglobal.upc.biz"
 }
 
-variable "vpc_cidr" {
-  default = "10.0.0.0/16"
+variable "jump_username" {
+  description = "Jump server user name"
+  default     = "ec2-user"
 }
 
-variable "vpc_azs" {
-  default = ["eu-central-1a", "eu-central-1b"]
+variable "app_username" {
+  description = "API server username"
+  default     = "ec2-user"
 }
 
-variable "vpc_private_subnets" {
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
+variable "app_ami_name" {
+  description = "Name of the ami to use for API servers"
+  default     = "packer-app*"
 }
 
-variable "vpc_public_subnets" {
-  default = ["10.0.3.0/28", "10.0.3.16/28"]
+variable "jump_ami_name" {
+  description = "Name of the ami to use for jump server"
+  default     = "packer-jump*"
+}
+
+variable "rapid7_ami_name" {
+  description = "Name of the rapid7 ami"
+  default     = "nexpose-pre-authorized-engine-*"
+}
+
+variable "ami_owner" {
+  description = "Owner of the RedHat ami"
+  default     = "536449617726"
+}
+
+variable "rapid7_ami_owner" {
+  description = "Owner of the ami"
+  default     = "679593333241"
 }
